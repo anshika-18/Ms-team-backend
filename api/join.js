ea904dc22251b15fd6dd3e913d72f76a2adddc91
anshika-18 <74226523+anshika-18@users.noreply.github.com>
2021-06-28 15:22:48 +0530

    test

---
+++ api/join.js
@@ -0,0 +1,88 @@
+module.exports=(app,rooms)=>{
+
+    const Room=require('../room')
+
+    app.get('/',(req,res)=>{
+        res.send('Hello World ..!!!')
+    })
+
+    app.post('/rooms',(req,res)=>{
+        const newRoom=new Room(req.body.author)
+        rooms.push(newRoom)
+        res.json({
+            roomId:newRoom.roomId
+        })
+    })
+    
+    app.get('/rooms/:roomId',(req,res)=>{
+        const room =rooms.find((existingRoom)=>existingRoom.roomId===req.params.roomId)
+        res.json({...room})
+    })
+    
+    app.post('/rooms/:roomId/join',(req,res)=>{
+        const { params, body } = req;
+        const roomIndex = rooms.findIndex(existingRooms => existingRooms.roomId === params.roomId);
+        let room = null;
+        
+        if (roomIndex > -1) {
+            room = rooms[roomIndex]
+            room.addParticipants(body.participant);
+            rooms[roomIndex] = room
+        }
+        res.json({ ...room.getInfo() })
+    })
+    
+    app.post('/api/send',(req,res)=>{
+    
+        let transporter=nodemailer.createTransport({
+            host:'smtp.gmail.com',
+            port:465,
+            secure:true,
+            service:'gmail',
+            auth:{
+                user:process.env.user,
+                pass:process.env.pass
+            }
+        })
+        //console.log(req.body)    
+    
+        var to=req.body.to.split(',');
+        //console.log(to)
+    
+        for(var i=0;i<to.length;i++)
+        {
+            var options={
+                to:to[i].trim(),
+                subject:"Link for meeting",
+                html:"<h3>Joining Link for meeting is </h3>"+"<h3 style='font-weight:bold'>"+req.body.url+"</h3>"
+            }
+    
+            transporter.sendMail(options,(error,info)=>{
+                if(error)
+                {
+                    return console.log(error)
+                }
+                console.log('Message sent : %s',info.messageId);
+                console.log('Preview URL: %s',nodemailer.getTestMessageUrl(info));
+                console.log('link sent')
+            })
+        }
+    
+        res.json('successfull')
+    
+    })
+    
+    
+    app.post('/api/getname',(req,res)=>{
+        console.log('getname - - ',req.body)
+        const room =rooms.find((existingRoom)=>existingRoom.roomId===req.body.roomId)
+        for(let i=0;i<room.participants.length;i++)
+        {
+            if(room.participants[i].id===req.body.id)
+            {
+                return res.json(room.participants[i].name)
+            }
+        }
+    })
+    
+}
--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 require('dotenv').config()
 const http=require('http')
 const express=require('express')
-
 const bodyParser=require('body-parser')
 const cors=require('cors')
-
 const nodemailer=require('nodemailer')
 const app=express()
 const mongoose=require('mongoose')
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/',(req,res)=>{
-    res.send('Hello World ..!!!')
-})
 
 const {Server} =require('socket.io')
 const server=http.createServer(app)
@@ -23,17 +18,17 @@ const io=new Server(server,{
     }
 })
 
-const Room=require('./room')
 
-const socketToPeerHashMap={} 
+const MapPeerIdWithSocket={} 
 const rooms=[] 
+
 io.on('connect',(socket)=>{
     socket.emit('get:peerId')
     console.log('connected')
 
     socket.on('send:peerId',(peerId)=>{
         //console.log('lets see if it is set--',peerId)
-        socketToPeerHashMap[socket.id]=peerId;
+        MapPeerIdWithSocket[socket.id]=peerId;
 
     }) 
     
@@ -57,12 +52,10 @@ io.on('connect',(socket)=>{
     })
 
     socket.on('disconnect',()=>{ 
-        console.log('disconnect')
-        const peerId=socketToPeerHashMap[socket.id]
-        console.log(peerId)
+        //console.log('disconnect')
+        const peerId=MapPeerIdWithSocket[socket.id]
+        //console.log(peerId)
         io.emit("user:left",peerId)
-        console.log(rooms)
-        //const roomIndex = rooms.findIndex(existingRoom =>{ existingRoom.participants.includes(peerId)});
         let roomIndex=-1;
         for(var i=0;i<rooms.length;i++)
         {
@@ -85,99 +78,18 @@ io.on('connect',(socket)=>{
       
 })
 
-
-
+//connect to mongoDb
 mongoose.connect(process.env.MONGO_URL,{ useUnifiedTopology: true,useNewUrlParser:true })
-        .then(()=>{
-            console.log('database connected')
-        })
-        .catch(error=>{
-            console.log(error)
-        })
+    .then(()=>{
+        console.log('database connected')
+    })
+    .catch(error=>{
+        console.log(error)
+})
 
+//api's
 require('./auth/authRoutes')(app)
-
-app.post('/rooms',(req,res)=>{
-    const newRoom=new Room(req.body.author)
-    rooms.push(newRoom)
-    res.json({
-        roomId:newRoom.roomId
-    })
-})
-
-app.get('/rooms/:roomId',(req,res)=>{
-    const room =rooms.find((existingRoom)=>existingRoom.roomId===req.params.roomId)
-    res.json({...room})
-})
-
-app.post('/rooms/:roomId/join',(req,res)=>{
-
-    const { params, body } = req;
-    const roomIndex = rooms.findIndex(existingRooms => existingRooms.roomId === params.roomId);
-    let room = null;
-    //console.log('room index - ',roomIndex)
-    if (roomIndex > -1) {
-        room = rooms[roomIndex]
-        room.addParticipants(body.participant);
-        rooms[roomIndex] = room
-    }
-    //console.log('room - ',rooms)
-
-    res.json({ ...room.getInfo() })
-})
-
-app.post('/api/send',(req,res)=>{
-
-    let transporter=nodemailer.createTransport({
-        host:'smtp.gmail.com',
-        port:465,
-        secure:true,
-        service:'gmail',
-        auth:{
-            user:process.env.user,
-            pass:process.env.pass
-        }
-    })
-    //console.log(req.body)    
-
-    var to=req.body.to.split(',');
-    //console.log(to)
-
-    for(var i=0;i<to.length;i++)
-    {
-        var options={
-            to:to[i].trim(),
-            subject:"Link for meeting",
-            html:"<h3>Joining Link for meeting is </h3>"+"<h3 style='font-weight:bold'>"+req.body.url+"</h3>"
-        }
-
-        transporter.sendMail(options,(error,info)=>{
-            if(error)
-            {
-                return console.log(error)
-            }
-            console.log('Message sent : %s',info.messageId);
-            console.log('Preview URL: %s',nodemailer.getTestMessageUrl(info));
-            console.log('link sent')
-        })
-    }
-
-    res.json('successfull')
-
-})
-
-
-app.post('/api/getname',(req,res)=>{
-    console.log('getname - - ',req.body)
-    const room =rooms.find((existingRoom)=>existingRoom.roomId===req.body.roomId)
-    for(let i=0;i<room.participants.length;i++)
-    {
-        if(room.participants[i].id===req.body.id)
-        {
-            return res.json(room.participants[i].name)
-        }
-    }
-})
+require('./api/join')(app,rooms)
 
 const PORT=process.env.PORT||5000;
 server.listen(PORT,()=>{
